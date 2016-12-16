29781ee5ec59888b4aff7e4a50477120cf327698
wangfeng <715667426@qq.com>
2016-12-16 18:16:20 +0800

    美化UI

--- Code/ElfActivateTool/stdafx.h
+++ Code/ElfActivateTool/stdafx.h
@@ -42,7 +42,7 @@
 
 
 
-#ifdef _UNICODE
+#ifndef _UNICODE
 #if defined _M_IX86
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #elif defined _M_X64
@@ -51,5 +51,6 @@
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #endif
 #endif
+
 
 
